35721f5a49698f8b7a671b5ba1306162bbd0c138
Yana0maybe0Shevchenko <142226235+Yana0maybe0Shevchenko@users.noreply.github.com>
2024-02-14 17:37:17 +0200

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,4 +1,5 @@
 function getShippingMessage(country, price, deliveryFee) {
+    const totalPrice = price + deliveryFee;
     const message = `Shipping to ${country} will cost ${totalPrice} credits`;
     return message; 
 }
